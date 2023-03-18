13e0f7a4a1d4c0792d5a0cb3157d551a34a44cf3
skinnyfads <skinnyfads@proton.me>
2023-03-18 20:34:00 +0700

    feat(getAccessToken): implement cache

--- src/getAccessToken.ts
+++ src/getAccessToken.ts
@@ -1,11 +1,24 @@
 import axios from "axios";
 
 async function getAccessToken(spotifyUrl: string) {
+  const currentTimestamp = Date.now();
+  const lastExpTimestamp = getAccessToken.lastExpTimestamp;
+
+  if (getAccessToken.lastToken && currentTimestamp < lastExpTimestamp) {
+    return getAccessToken.lastToken;
+  }
   const response = await axios.get<string>(spotifyUrl);
-  const pattern = /accessToken":"(.*?)(?=")/g;
-  const accessToken = pattern.exec(response.data)![1];
+  const accessTokenPattern = /accessToken":"(.*?)(?=")/;
+  const expTimestampPattern = /accessTokenExpirationTimestampMs":(.*?)(?=,)/;
+  const accessToken = accessTokenPattern.exec(response.data)![1];
+  const expTimestamp = Number(expTimestampPattern.exec(response.data)![1]);
+
+  getAccessToken.lastExpTimestamp = expTimestamp;
+  getAccessToken.lastToken = accessToken;
 
   return accessToken;
 }
+getAccessToken.lastToken = "";
+getAccessToken.lastExpTimestamp = Date.now();
 
 export default getAccessToken;
