7fa0df5a5c8bdd109be047a8fd22be04e1b8def3
skinnyfads <skinnyfads@proton.me>
2023-03-31 06:13:59 +0700

    test(config): remove comment

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
   preset: "ts-jest/presets/default-esm",
-  // extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
